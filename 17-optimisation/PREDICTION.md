5c5b0cd09c3b33a0ba91c499a4ba616ed8348ba7
Raphael <raphalizier2@gmail.com>
2025-12-10 23:45:31 +0100

    Exercice 17

--- 17-optimisation/PREDICTION.md
+++ 17-optimisation/PREDICTION.md
@@ -2,14 +2,18 @@
 
 ## Fonction `moyenneInef`
 
-**Problème identifié :** ___
+**Problème identifié :** La fonction recalcule la somme complète à chaque itération plutot que d'ajouter t[i] à une somme globale
 
-**Nombre d'opérations actuelles (pour n éléments) :** ___
+**Nombre d'opérations actuelles (pour n éléments) :** n×n/2
 
 **Version optimisée :**
 ```java
 public static double moyenneEff(int[] t) {
-    
+    int somme = 0;
+    for (int i = 0; i < t.length; i++) {
+        somme += t[i];
+    }
+    return (double) somme / t.length;
 }
 ```
 
@@ -17,12 +21,19 @@ public static double moyenneEff(int[] t) {
 
 ## Fonction `contientDoublonInef`
 
-**Problème identifié :** ___
+**Problème identifié :** La fonction compare chaque paire 2 fois (i/j puis j/i)
 
 **Version optimisée :**
 ```java
 public static boolean contientDoublonEff(int[] t) {
-    
+    for (int i = 0; i < t.length; i++) {
+        for (int j = i + 1; j < t.length; j++) {
+            if (t[i] == t[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 ```
 
@@ -30,12 +41,14 @@ public static boolean contientDoublonEff(int[] t) {
 
 ## Fonction `premierEtDernierInef`
 
-**Problème identifié :** ___
+**Problème identifié :** La fonction parcourt 2 fois le tableau alors qu'il n'est même pas nécessaire de faire une boucle pour récupérer les informations demandées
 
 **Version optimisée :**
 ```java
 public static String premierEtDernierEff(int[] t) {
-    
+    int premier=t[0];
+    int dernier=t[t.length-1];
+    return premier + " et " + dernier;
 }
 ```
 
@@ -43,11 +56,16 @@ public static String premierEtDernierEff(int[] t) {
 
 ## Fonction `rechercheInef`
 
-**Problème identifié :** ___
+**Problème identifié :** La fonction continue même si le résultat demandé est apparu, et si il apparait plusieurs fois, c'est l'index de la dernière occurence qui sera retenu
 
 **Version optimisée :**
 ```java
 public static int rechercheEff(int[] t, int val) {
-    
+    for (int i = 0; i < t.length; i++) {
+        if (t[i] == val) {
+            return i;
+        }
+    }
+    return -1;
 }
 ```
--- 17-optimisation/src/Main.java
+++ 17-optimisation/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nombres = {3, 7, 2, 8, 5, 1, 9, 4};
+        int[] nombres = { 3, 7, 2, 8, 5, 1, 9, 4 };
 
         System.out.println("moyenneInef : " + moyenneInef(nombres));
         System.out.println("contientDoublonInef : " + contientDoublonInef(nombres));
@@ -10,22 +10,18 @@ public class Main {
 
     // PROBLEME 1 : Calcule la somme à chaque itération
     public static double moyenneInef(int[] t) {
-        double moyenne = 0;
+        int somme = 0;
         for (int i = 0; i < t.length; i++) {
-            int somme = 0;
-            for (int j = 0; j <= i; j++) {
-                somme = somme + t[j];
-            }
-            moyenne = (double) somme / (i + 1);
+            somme += t[i];
         }
-        return moyenne;
+        return (double) somme / t.length;
     }
 
     // PROBLEME 2 : Compare chaque paire deux fois
     public static boolean contientDoublonInef(int[] t) {
         for (int i = 0; i < t.length; i++) {
-            for (int j = 0; j < t.length; j++) {
-                if (i != j && t[i] == t[j]) {
+            for (int j = i + 1; j < t.length; j++) {
+                if (t[i] == t[j]) {
                     return true;
                 }
             }
@@ -35,26 +31,18 @@ public class Main {
 
     // PROBLEME 3 : Parcourt le tableau deux fois
     public static String premierEtDernierInef(int[] t) {
-        int premier = 0;
-        for (int i = 0; i < t.length; i++) {
-            premier = t[i];
-            break;
-        }
-        int dernier = 0;
-        for (int i = 0; i < t.length; i++) {
-            dernier = t[i];
-        }
+        int premier = t[0];
+        int dernier = t[t.length - 1];
         return premier + " et " + dernier;
     }
 
     // PROBLEME 4 : Continue après avoir trouvé
     public static int rechercheInef(int[] t, int val) {
-        int index = -1;
         for (int i = 0; i < t.length; i++) {
             if (t[i] == val) {
-                index = i;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
 }
